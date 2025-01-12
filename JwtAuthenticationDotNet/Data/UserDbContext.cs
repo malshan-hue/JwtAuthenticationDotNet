ad3ce87ff7b5af152e4a8d65290f07cf39e83365
Malshan Rathnayake <malshan.edu@gmail.com>
2025-01-12 23:37:41 +0530

    1. added entity framework and db context

---
+++ JwtAuthenticationDotNet/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuthenticationDotNet.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthenticationDotNet.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
