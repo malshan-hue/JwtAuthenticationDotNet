403a2c90fa8601426259ed1c7aea01d49a1aa726
Malshan Rathnayake <malshan.edu@gmail.com>
2025-01-12 22:45:07 +0530

    login and register controller test

---
+++ JwtAuthenticationDotNet/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using JwtAuthenticationDotNet.Entities;
+using JwtAuthenticationDotNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthenticationDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public IActionResult Register(UserDtop request)
+        {
+            var hasdedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.UserName = request.UserName;
+            user.PasswordHash = hasdedPassword;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserDtop request)
+        {
+            if(user.UserName != request.UserName)
+            {
+                return BadRequest("User Not Found");
+            }
+
+            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Invalid password");
+            }
+
+            var token = "success";
+
+            return Ok(token);
+        }
+    }
+}
---
+++ JwtAuthenticationDotNet/Models/UserDtop.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthenticationDotNet.Models
+{
+    public class UserDtop
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
